7a8a1b7041d8b4b0c64591a534d3621df2b959e8
daniel <danielddemissie@gmail.com>
2023-04-09 15:43:28 +0300

    init: initialize the project

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import styles from "../styles/Home.module.css";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <button className={styles.btn}>Home</button>
+    </main>
+  );
+}
